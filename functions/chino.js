5cec01f62b8f97ecc4a9d686b7861447e3f086c3
xix556 <lamngan1705@gmail.com>
2024-07-20 21:10:32 +0700

    updates

--- functions/chino.js
+++ functions/chino.js
@@ -1,6 +1,6 @@
 export async function onRequestGet() {
     const max = 267;
-    return new Response(await fetch(`https://raw.githubusercontent.com/AutumnVN/chino.webp/main/${Math.ceil(Math.random() * max)}.webp`).then(r => r.blob()), {
+    return new Response(await fetch(`https://raw.githubusercontent.com/kawaiicassie/waifu/main/${Math.ceil(Math.random() * max)}.png`).then(r => r.blob()), {
         headers: {
             'content-type': 'image/webp',
             'cache-control': 'no-cache, no-store, must-revalidate, max-age=0'
